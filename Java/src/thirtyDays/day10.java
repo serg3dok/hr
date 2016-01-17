91f40a50a735db4ece5b121c31a04cee647acba4
Sergey Okinchuk <serg3d@hotmail.com>
2016-01-16 22:00:55 -0800

    StringBuffer solution

--- Java/src/thirtyDays/day10.java
+++ Java/src/thirtyDays/day10.java
@@ -1,6 +1,7 @@
 package thirtyDays;
 
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 /**
  * Created by Sergey on 1/12/2016.
@@ -20,17 +21,52 @@ public class day10 {
 
 
     }
-    static String toBinary(int num) {
+     static String toBinary(int num) {
         //return Integer.toBinaryString(x); // Integer method
-        String last = "";
-        while (num > 0) {
-            int mod = num % 2;
-            num = num / 2;
-            String binary = Integer.toString(mod);
-            last = mod + last;
-        }
+        //String binaryResult = "0";
+         StringBuilder binaryResult = new StringBuilder("0");
 
-        return last;
+         int i = 0;
+         while (num > 0)
+         {
+
+             i = binaryResult.length()-1;
+             while (i > -1)
+             {
+                 if (binaryResult.charAt(i) == '0')
+                 {
+                     binaryResult.setCharAt(i, '1');
+                     if (i == 0)
+                     {
+                         continue;
+                     }
+
+                 }
+                 else if(binaryResult.charAt(i) == '1')
+                 {
+                     binaryResult.setCharAt(i, '0');
+
+
+                     if (i == 0)
+                     {
+                         binaryResult.insert(0, '1');
+                     }
+
+                 }
+
+                 i--;
+             }
+
+
+
+             num--;
+         }
+
+
+
+
+
+        return binaryResult.toString();
     }
 
 
