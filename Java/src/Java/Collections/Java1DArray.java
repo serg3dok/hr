c1cbdd837d250943b8fec520bef9b6ba6817dbad
Sergey Okinchuk <serg3d@hotmail.com>
2016-02-23 18:24:55 -0800

    updated Java 1D Array (Java Collections)

--- Java/src/Java/Collections/Java1DArray.java
+++ Java/src/Java/Collections/Java1DArray.java
@@ -9,27 +9,27 @@ public class Java1DArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.next();
         int[] numbers = new int[n];
-        int i = 0;
-        while (sc.hasNext())
-        {
+
+        for (int i = 0; i < n; i++) {
             numbers[i] = sc.nextInt();
-            i++;
         }
 
+
+
+
         int result = 0;
-        int sum = numbers[0];
-        for (i = 0; i < n; i++) {
+
+        for (int i = 0; i < n; i++) {
+            int sum = numbers[i];
+            if (sum < 0) result += 1;
             for (int j = i+1; j < n - 1; j++) {
-                if (sum + numbers[j] < 0) {
-                    sum = sum + numbers[j];
+                int nextNum = numbers[j];
+                if (sum + nextNum <= 0) {
+                    sum = sum + nextNum;
                     result = result + 1;
                 }
-                else
-                {
-                    break;
-                }
+
             }
         }
 
