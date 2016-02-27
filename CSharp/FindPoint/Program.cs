df8964aaf5cad71b10fd8ded22b5fed08de00613
Sergey Okinchuk <serg3d@hotmail.com>
2016-02-26 16:59:32 -0800

    updated Find Point

--- CSharp/FindPoint/Program.cs
+++ CSharp/FindPoint/Program.cs
@@ -11,46 +11,49 @@ namespace FindPoint
     {
         static void Main(string[] args)
         {
-            int t = Console.Read();
-            int[] result = new int[2];
+            int t = Convert.ToInt32(Console.ReadLine());
 
             for (int i = 0; i < t; i++)
             {
-                int[,] points = new int[2,2];
-                for (int j = 0; j < 2; j++)
-                {
-                    for (int k = 0; k < 2; k++)
-                    {
-                        points[j,k] = Console.Read();
-                    }
-                }
+                string[] numbersInput = Console.ReadLine().Split(' ');
+                int l = numbersInput.Length;
+                int[] points = new int[4];
+                points = Array.ConvertAll(numbersInput, Int32.Parse);
+                int[] result = new int[2];
+
+
 
                 // Calculate x
-                if (points[0,0] < points[1,0])
+                if (points[0] < points[2])
                 {
-                    result[0] = points[0,0] + points[1,0]; 
+                    if (points[0] > 0)
+                    {
+                        result[0] = points[2] + (points[2] - points[0]);
+                    }
+                    else
+                    {
+                        result[0] = points[2] + (points[2] + (points[2]-(-points[0])));
+                    }
+                    
                 }
                 else
                 {
-                    result[0] = points[0,0] - points[1,0]; 
+                    result[0] = points[2] - (points[2] + points[0]);
                 }
 
                 // Calculate y
-                if (points[0,0] < points[0,1])
+                if (points[1] < points[3])
                 {
-                    result[1] = points[0,0] + points[0,1];
+                    result[1] = points[3] + (points[3] - points[1]);
                 }
                 else
                 {
-                    result[1] = points[0,0] - points[0,1];
+                    result[1] = points[3] - (points[3] + points[1]);
                 }
-                
+
                 Console.WriteLine(result[0] + " " + result[1]);
-
             }
-
-            
-
         }
     }
 }
+
