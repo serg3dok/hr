9316695c1880bdf11ce95ebcd5c3ab4803ff6b42
Sergey Okinchuk <serg3d@hotmail.com>
2016-03-01 00:14:58 -0800

    added Java Stack (Java Collections)

---
+++ Java/src/Java/Collections/JavaStack.java
@@ -0,0 +1,56 @@
+package Java.Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class JavaStack {
+    private ArrayList<String> Stack;
+
+
+    JavaStack() {
+        Stack = new ArrayList<String>();
+    }
+
+
+    public static void main(String []argh)
+    {
+        Stack MyStack = new Stack
+
+        Scanner sc = new Scanner(System.in);
+
+
+        while (sc.hasNext()) {
+            String input=sc.next();
+            //Complete the code
+
+
+
+
+        }
+
+    }
+
+    public void push(String s) {
+
+    }
+
+    public int pop() {
+        String r =
+
+        return
+
+    }
+    public void peek() {
+
+    }
+
+    public void isEmpty() {
+
+    }
+
+    public void isFull()  {
+
+    }
+}
