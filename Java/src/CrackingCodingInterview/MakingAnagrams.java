c4dbed7dd5021c185793f0cec70837554327d7e0
Sergey Okinchuk <serg3d@hotmail.com>
2016-10-01 23:04:27 -0700

    Making Anagrams in progress

--- Java/src/CrackingCodingInterview/MakingAnagrams.java
+++ Java/src/CrackingCodingInterview/MakingAnagrams.java
@@ -1,5 +1,6 @@
 package CrackingCodingInterview;
 
+
 import java.util.Scanner;
 
 /**
@@ -7,21 +8,29 @@ import java.util.Scanner;
  */
 public class MakingAnagrams {
 
-    //      i
-    // abcfqe
-    //      j
-    // sfswrh
-    // dd ee gg
-    // 12
+    //    i
+    // abcfqegog
+    //  j
+    // sfswrhgnyfgo
+    //
+    // 21
+    //
 
 
     public static int numberNeeded(String first, String second) {
         StringBuilder a = new StringBuilder(first);
         StringBuilder b = new StringBuilder(second);
 
+        //System.out.println(a.charAt(3) == b.charAt(1));
+
+
+
+
         for (int i = 0; i < a.length(); i++) {
-            for (int j = i+1; j < b.length(); j++) {
+            for (int j = 0; j < b.length(); j++) {
+
                 if (a.charAt(i) == b.charAt(j)) {
+                    //System.out.println("here");
                     a.deleteCharAt(i);
                     b.deleteCharAt(j);
                     continue;
@@ -29,14 +38,24 @@ public class MakingAnagrams {
             }
         }
 
+        System.out.println(a);
+        System.out.println(b);
+
         int toRemove = a.length() + b.length();
         return toRemove;
     }
 
+
+    // remove char from string
+    public static String removeCharFromString(String str, int index) {
+
+        return str;
+    }
+
     public static void main(String[] args) {
 
-        String a = "cde";
-        String b = "abc";
+        String a = "abcfqe";
+        String b = "sfswrh";
         System.out.println(numberNeeded(a, b));
     }
 
