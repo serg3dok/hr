91e19dfbc6c681ae60761224762f46a25ac10bab
Sergey Okinchuk <serg3d@hotmail.com>
2016-10-17 11:43:30 -0700

    Anagrams solved

--- CSharp/MakingAnagrams/Program.cs
+++ CSharp/MakingAnagrams/Program.cs
@@ -11,9 +11,20 @@ namespace MakingAnagrams
     {
         static void Main(string[] args)
         {
-            string str1 = "dn";
-            string str2 = "n";
-            // 1
+            /*
+               i
+            fsqoaidfaukvngpsugszsnseskicpejjvytviya
+            kmgsxamduovigbasjchnoskolfwjhgetnmnkmcphmpwnrrwtymjtwxget
+            100
+            40
+
+
+
+            */
+
+            string str1 = "fsqoiaidfaukvngpsugszsnseskicpejjvytviya";
+            string str2 = "ksmfgsxamduovigbasjchnoskolfwjhgetnmnkmcphqmpwnrrwtymjtwxget";
+            // 42
 
             int result = anagram(str1, str2);
             Console.WriteLine(result);
@@ -25,34 +36,50 @@ namespace MakingAnagrams
         {
             if (str1.Equals(str2)) return 0;
 
-            int matches = 0;
+            
+            //int matches = 0;
+            int length = str1.Length + str2.Length;
+            Console.WriteLine("length: " + length);
+
+            
+
 
             if (str1.Length >= str2.Length)
             {
+                string bigger = str1;
+
                 for (int i = 0; i < str2.Length; i++)
                 {
-                    if (str1.Contains(str2[i]))
+                    if (bigger.Contains(str2[i]))
                     {
-                        str1.Remove(str1.IndexOf(str2[i]));
-                        matches++;
+                        
+                        bigger.Remove(str1.IndexOf(str2[i]));
+                        length = length - 2;
                     }
-                    
                 }
             }
             else
             {
+                string bigger = str2;
                 for (int i = 0; i < str1.Length; i++)
                 {
-                    if (str2.Contains(str1[i]))
+                    if (bigger.Contains(str1[i]))
                     {
-                        str2.Remove(str2.IndexOf(str1[i]));
-                        matches++;
+                        Console.WriteLine("str1[" + i + "] = " + str1[i]);
+                        Console.WriteLine("str2[" + str2.IndexOf(str1[i]) + "] = " + str2[str2.IndexOf(str1[i])]);
+                        Console.WriteLine(bigger);
+                        //bigger.Remove(str2.IndexOf(str1[i]));
+                        bigger.Replace(str1[i].ToString(), "0");
+                        Console.WriteLine(bigger);
+                        length = length - 2;
+                        Console.WriteLine("length = " + length);
+                        Console.WriteLine();
                     }
                 }
             }
             
             
-            return (str1.Length + str2.Length) - matches * 2;
+            return length;
         }
     }
 }
