2a6401d6671ee672f79783118c86bba4cd80c4e0
Sergey Okinchuk <serg3d@hotmail.com>
2016-10-20 17:40:04 -0700

    working on TaleOfTwoStacks (Queues)

--- Java/src/CrackingCodingInterview/TaleOfTwoStacks.java
+++ Java/src/CrackingCodingInterview/TaleOfTwoStacks.java
@@ -1,8 +1,50 @@
 package CrackingCodingInterview;
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 /**
  * Created by Sergey on 10/20/2016.
  */
 public class TaleOfTwoStacks {
+    public static class MyQueue<T> {
+        Stack<T> stackNewestOnTop = new Stack<T>();
+        Stack<T> stackOldestOnTop = new Stack<T>();
 
+        public void enqueue(T value) { // Push onto newest stack
+            stackNewestOnTop.push(value);
+            stackOldestOnTop.push(value);
+        }
+
+        public T peek() {
+            return stackNewestOnTop.peek();
+        }
+
+        public T dequeue() {
+            return stackNewestOnTop.pop();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        MyQueue<Integer> queue = new MyQueue<Integer>();
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int operation = scan.nextInt();
+            if (operation == 1) { // enqueue
+                queue.enqueue(scan.nextInt());
+            } else if (operation == 2) { // dequeue
+                queue.dequeue();
+            } else if (operation == 3) { // print/peek
+                System.out.println(queue.peek());
+            }
+        }
+        scan.close();
+    }
 }
